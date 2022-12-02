9f5f3974d8e927d27125d3aef25e581eb0221383
“antoanparashkevov” <tonkata1505@gmail.com>
2022-12-02 16:02:56 +0200

    Fetching route parameters reactively by subscribing to the params observable

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-user',
@@ -12,6 +12,18 @@ export class UserComponent implements OnInit {
   constructor(private currentRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.currentRoute.snapshot.params.id,
+      name: this.currentRoute.snapshot.params.name
+    }
+    this.currentRoute.params
+      .subscribe
+      (
+        (param: Params) => {
+          this.user.id = param.id;
+          this.user.name = param.name;
+        }
+        )
     console.log('entered id param >>> ', this.currentRoute.snapshot.params['id'])
     console.log('all dynamic route segments >>> ', this.currentRoute.snapshot.params)
   }
