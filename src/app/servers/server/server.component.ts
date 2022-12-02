72664a539229c55fd8bd3f8313dd031178d872b4
“antoanparashkevov” <tonkata1505@gmail.com>
2022-12-02 16:44:50 +0200

    Dynamically pages

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-server',
@@ -10,10 +11,20 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private currentRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = this.currentRoute.snapshot.params['id']
+    this.server = this.serversService.getServer(Number(id));
+
+    this.currentRoute.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(Number(params['id']))
+      })
+
   }
 
 }
